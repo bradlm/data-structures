8a3556b179c3a36df48e13fda0fbbda1c69561fa
bradlm <bradmelluish@gmail.com>
2016-07-27 18:55:55 -0700

    Implement prototypal queue and stack
    Wow! 2 in 1.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueInstance = Object.create(queueMethods);
+  queueInstance.enqueuePointer = 0;
+  queueInstance.dequeuePointer = 0;
+  queueInstance.length = 0;
+  queueInstance.storage = {};
+  return queueInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.enqueuePointer++] = value;
+  this.length++;
+};
 
+queueMethods.dequeue = function() {
+  if (this.length > 0) {
+    this.length--;
+    return this.storage[this.dequeuePointer++];
+  }
+};
+
+queueMethods.size = function() {
+  return this.length;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stackInstance = Object.create(stackMethods);
+  stackInstance.length = 0;
+  stackInstance.storage = {};
+  return stackInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.length++] = value;
+};
 
+stackMethods.pop = function() {
+  return this.length > 0 ? this.storage[--this.length] : null;
+};
+
+stackMethods.size = function() {
+  return this.length;
+};
