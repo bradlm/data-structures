416f3d0e69a03c3acb42b6561df807f22e893da9
bradlm <bradmelluish@gmail.com>
2016-07-27 19:22:26 -0700

    Implement Pseudoclassical Queue and Stack Object Types It was great. We learned all the things.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.enqueuePointer = 0;
+  this.dequeuePointer = 0;
+  this.storage = {};
 };
 
+Queue.prototype.size = function() {
+  return this.length;
+};
 
+Queue.prototype.enqueue = function(value) {
+  this.length++;
+  this.storage[this.enqueuePointer++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.length > 0) {
+    this.length--;
+    return this.storage[this.dequeuePointer++];
+  }
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.length++] = value;
+};
 
+Stack.prototype.pop = function() {
+  return this.length > 0 ? this.storage[--this.length] : null;
+};
+
+Stack.prototype.size = function() {
+  return this.length;
+};
