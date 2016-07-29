022c41bd00cd6301b0e5a8afa9b60a86c3a58734
bradlm <bradmelluish@gmail.com>
2016-07-28 19:42:42 -0700

    Implement a graph
    Time complexity analysis included at bottom.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,27 @@
 // Instantiate a new graph
 var Graph = function() {
   this.nodeList = {};
+  this.size = 0;
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
-  this.nodeList[node] = new GNode(node); 
+Graph.prototype.addNode = function(key) {
+  this.nodeList[key] = new GNode(key);
+  this.size++;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
-  return this.nodeList.hasOwnProperty(node);
+Graph.prototype.contains = function(key) {
+  return this.nodeList.hasOwnProperty(key);
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
-  for (var neighbor in this.nodeList[node].edges) {
-    this.nodeList[neighbor].removeEdge(node);
+Graph.prototype.removeNode = function(key) {
+  for (var neighbor in this.nodeList[key].edges) {
+    this.nodeList[neighbor].removeEdge(this.nodeList[key]);
   }
-  delete this.nodeList[node];
+  delete this.nodeList[key];
+  this.size--;
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -28,16 +31,21 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.nodeList[fromNode].addEdge(toNode);
-  this.nodeList[toNode].addEdge(fromNode);
+  this.nodeList[fromNode].addEdge(this.nodeList[toNode]);
+  this.nodeList[toNode].addEdge(this.nodeList[fromNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this.nodeList[fromNode].removeEdge(this.nodeList[toNode]);
+  this.nodeList[toNode].removeEdge(this.nodeList[fromNode]);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodeList) {
+    cb(key);
+  }
 };
 
 
@@ -57,4 +65,11 @@ GNode.prototype.removeEdge = function(node) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+// addNode = technically, linear b/c of potential hashtable collisions. Otherwise on average, constant time.
+// contains = constant. but technically, if the hashtable is terribly implemented then it could lead to constant time.
+// removeNode = linear
+// hasEdge = linear unless the object uses a hashtable to keep track of all keys
+// addEdge = linear
+// removeEdge = linear
+// forEachNode = depends on the complexity of the callback. At least linear.
 
