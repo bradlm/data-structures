22a05cb887e2e3fd0f8bc5dbf6866eff8bd324dc
bradlm <bradmelluish@gmail.com>
2016-07-29 16:28:17 -0700

    Implement BST.
    Added a new keyword to the testing to complete Psuedoclassical style.
    Implemented BST without balancing. Time complexity analysis at bottom of document.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,57 @@
 var BinarySearchTree = function(value) {
+  this.value = value; 
+  this.left = this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  (function insertNode(node) {
+    if (node === null) {
+      return new BinarySearchTree(value);
+    }
+    var childNode;
+    if (value < node.value) { 
+      if (childNode = insertNode(node.left)) {
+        node.left = childNode;
+      }
+    } else {
+      if (childNode = insertNode(node.right)) {
+        node.right = childNode;
+      }
+    }
+  }).call(null, this); //null is unneeded context
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  return (function checkContains(node) {
+    if (node === null) {
+      return false;
+    }
+    if (node.value === value) {
+      return true;
+    }
+    if (value < node.value) {
+      return checkContains(node.left);
+    }
+    return checkContains(node.right);
+  }).call(null, this);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  return (function depthFirstAWESOME(node) {
+    if (node === null) {
+      return;
+    }
+    cb(node.value);
+    depthFirstAWESOME(node.left);
+    depthFirstAWESOME(node.right);
+  }).call(null, this);
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ insert = logarithmic if balanced, else linear
+ contains = logarithmic if balanced, else linear
+ depthFirstLog = linear
+
  */
