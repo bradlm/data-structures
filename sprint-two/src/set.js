7e11c6e13c7c740ff2ec0d9f645b0b82a62d4b53
bradlm <bradmelluish@gmail.com>
2016-07-28 19:55:13 -0700

    Implement Set Structure
    Only handles strings.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,21 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  set._storage = undefined;
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return Boolean(this._storage[item]); 
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+add = constant on average, worst case linear if lots of collisions
+contains = constant or linear
+remove = constant or linear
+
  */
